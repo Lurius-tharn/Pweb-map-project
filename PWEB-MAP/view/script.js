16f1d5832c1ca334affc379621674ffd070669ee
Kvpso270 <60976385+Kvpso270@users.noreply.github.com>
2021-03-01 11:30:04 +0100

    Add files via upload

---
+++ PWEB-MAP/view/script.js
@@ -0,0 +1,10 @@
+function initialize() {
+    var map = L.map('map').setView([48.833, 2.333], 7); // LIGNE 18
+
+    var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { // LIGNE 20
+        attribution: '© OpenStreetMap contributors',
+        maxZoom: 19
+    });
+
+    map.addLayer(osmLayer);
+}
